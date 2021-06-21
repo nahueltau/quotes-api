1b94f1e219376a52319d5fee71eecf26f69f3673
nahueltau <nahue.tau@gmail.com>
2021-06-21 17:10:23 -0300

    Quote model, migration, controller and factory created

---
+++ database/factories/QuoteFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Quote;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuoteFactory extends Factory
+{
+    protected $model = Quote::class;
+
+    public function definition(): array
+    {
+    	return [
+    	    //
+    	];
+    }
+}
